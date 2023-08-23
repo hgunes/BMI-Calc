c4125b7cb24039b282c8422fde70c5adb46e9e10
harungunes <h.gunes13@gmail.com>
2023-08-23 22:23:57 +0200

    init repo

---
+++ BMI-CalcUITests/BMI_CalcUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BMI_CalcUITestsLaunchTests.swift
+//  BMI-CalcUITests
+//
+//  Created by Harun Gunes on 23/08/2023.
+//
+
+import XCTest
+
+final class BMI_CalcUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
